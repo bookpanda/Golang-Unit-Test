311272a72ee0d763e7e82f21c079750dcd6d1acb
Suppawich Tawornpichayachai <upsaga77@hotmail.com>
2025-02-10 17:13:12 +0700

    Implement leapyear test

--- main.go
+++ main.go
@@ -1,16 +1,4 @@
 package main
 
-import (
-	"Golang-Unit-Test/leapyear"
-	"fmt"
-)
-
 func main() {
-    for year := 2000; year <= 2100; year++ {
-        if leapyear.IsLeapYear(year) {
-            fmt.Printf("%d is a leap year\n", year)
-        } else {
-            fmt.Printf("%d is not a leap year\n", year)
-        }
-    }
 }
