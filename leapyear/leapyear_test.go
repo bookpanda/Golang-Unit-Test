180fa595ddc83b98374624f1722fd632ec9b8185
Suppawich Tawornpichayachai <upsaga77@hotmail.com>
2025-02-10 17:25:02 +0700

    Remove unused file and comment

--- leapyear/leapyear_test.go
+++ leapyear/leapyear_test.go
@@ -9,11 +9,11 @@ func TestIsLeapYear(t *testing.T) {
 		year     int
 		expected bool
 	}{
-		// {2000, true},  // Divisible by 400
-		// {1900, false}, // Divisible by 100 but not by 400
+		{2000, true},  // Divisible by 400
+		{1900, false}, // Divisible by 100 but not by 400
 		{2024, true},  // Divisible by 4 but not by 100
 		{2023, false}, // Not divisible by 4
-		// {-4, false},   // Negative year (invalid case)
+		{-4, false},   // Negative year (invalid case)
 	}
 
 	for _, tt := range tests {
