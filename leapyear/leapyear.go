c63cf93a6085b6cd4883ffec5e1345801c756048
Khar <kharkkr@gmail.com>
2025-02-11 00:31:26 +0700

    Update leaoyear logic

--- leapyear/leapyear.go
+++ leapyear/leapyear.go
@@ -6,7 +6,10 @@ func IsLeapYear(year int) bool {
     }
     if year%4 == 0 {    
         if year%100 == 0 {
-            return year%400 == 0
+            if year%400 == 0 {
+                return true
+            }
+            return false
         }
         return true
     }
